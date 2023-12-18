ca6a16f5613f86954b806926f4ef82efcc01680f
ark101202 <arkeldi@tamu.edu>
2023-12-17 19:46:57 -0600

    Implemented register command to allow user to link up discord account to codeforces account, using temporary memory (python dictionary) for mapping. Implemented Codeforces API to verify registered username.

--- main.py
+++ main.py
@@ -17,10 +17,55 @@ async def on_ready(): #when bot is ready to recieve commands
 async def duel(ctx): # command after ! 
     await ctx.send("1v1 me bro") #bot will send message to channel
 
-client.run('MTE4NTc0MDYzODY1MDYzNDM0MA.GYvgGU.c2kPcWp8C7ZTE6mNuS8Kz0ysSLOQaxftNJOXzE') #run bot, update each time you change sthm in discord dev portal 
-#link up this code with discord token 
-
 #run python3 main.py in terminal to run bot
+#/Applications/Python\ 3.9/Install\ Certificates.command
 
 
 
+#---------------------------------------------------------------------------------------------------------------------------------------------------------------------#
+#---------------------------------------------------------------------------------------------------------------------------------------------------------------------#
+
+
+#Register discord handle to codeforces handle:
+
+#Register user command here:, going the temp memory route, just python dict 
+#key,value dictionary for discord -> codeforces handle
+
+userDict = {}
+
+@client.command() # register command: !register codeforcesUsername
+async def register(ctx, codeforcesHandle: str):
+    #check if user is registered ?
+    discordHandle = str(ctx.author.id)
+    discordServer = str(ctx.guild.id) #make str here is using json later
+
+    #check if codeforces handle is valid
+    if not verifyCodeforcesHandle(codeforcesHandle):
+        await ctx.send("Incorrect Codeforces Username, check again silly goose")
+        return
+
+    discordHandle = f"{ctx.author.name}#{ctx.author.discriminator}"
+
+    if discordServer not in userDict: #check is server is in dict
+        userDict[discordServer] = {} 
+    
+    #update map for user
+    userDict[discordServer][discordHandle] = codeforcesHandle
+    
+    await ctx.send("You have registered " + codeforcesHandle + " with " + discordHandle)
+
+
+# making api call to codeforces to verify handle
+# pip3 install requests 
+import requests
+
+def verifyCodeforcesHandle(codeforcesHandle):
+    response = requests.get("https://codeforces.com/api/user.info?handles=" + codeforcesHandle)
+    if response.status_code == 200:
+        return True 
+    else:
+        return False
+
+
+client.run('') #run bot, update each time you change sthm in discord dev portal, 
+#this must be the final line 
